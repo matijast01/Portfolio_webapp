3f8e2efb38b6b31a0a65fe3fc9a6a10121ad30a9
Matijast <matijastipcevic01@yahoo.com>
2022-11-22 15:16:16 +0000

    Add navigation menu and contact us page

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content2)
 
-col3,  empty_col,col4 = st.columns([1.5, 0.5, 1.5])
+col3,  empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
