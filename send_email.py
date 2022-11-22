70178631e0f582da65fe0fc0cc71bc3d4b1706a1
Matijast <matijastipcevic01@yahoo.com>
2022-11-22 16:15:08 +0000

    Add send email function and connect to form

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+import ssl
+import streamlit as st
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = st.secrets["EMAIL"]
+    password = st.secrets["PASSWORD"]
+
+    receiver = st.secrets["EMAIL"]
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
